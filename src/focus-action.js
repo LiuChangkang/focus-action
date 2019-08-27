be5080ac97e6aee0f28e5bebe88b14288e61bbf1
LiuChangkang <19881843@qq.com>
2019-08-27 20:33:20 +0800

    修复安卓下的BUG和优化

--- src/focus-action.js
+++ src/focus-action.js
@@ -2,17 +2,19 @@
  * Created by 刘昌康 on 2019/8/19.
  * focus-action
  */
-window.addEventListener('DOMContentLoaded', function () {
+setTimeout(function () {
+  var document = window.document;
   var sUserAgent = navigator.userAgent.toLowerCase();
   var isIos = /iphone/.test(sUserAgent) || /ipad/.test(sUserAgent);
-  var isAndroid = !isIos && /android/.test(sUserAgent);
+  var isAndroid = /android/.test(sUserAgent);
   var waitResize = false; // 获取焦点时等待resize事件的开关
 
   // 有元素获取焦点时把元素，如果不在可视区域就将元素滚动至可视区域
   document.addEventListener('focusin', function (e) {
     setTimeout(function () {
-      if (e.target.getBoundingClientRect().bottom > document.documentElement.clientHeight || e.target.getBoundingClientRect().top < 0)
-        e.target.scrollIntoView(false);
+      if (e.target.getBoundingClientRect().bottom > document.documentElement.clientHeight || e.target.getBoundingClientRect().top < 0) {
+        e.target.scrollIntoView(isAndroid);
+      }
     }, 300);
   });
 
@@ -32,25 +34,24 @@ window.addEventListener('DOMContentLoaded', function () {
 
     // ios点击输入框迟钝和不反应的解决
     document.addEventListener('click', function (e) {
-      if (/INPUT|TEXTAREA/i.test(e.target.tagName))
-        e.target.focus();
+      if (/INPUT|TEXTAREA/i.test(e.target.tagName)) e.target.focus();
     });
   } else if (isAndroid) {
     // 添加html向上偏移的class
     var style = document.createElement('style');
     style.type = 'text/css';
-    style.innerHTML = 'body.focus-action-up{transform:translate(0,-33vh);-webkit-transform:translate(0,-33vh)}';
+    style.innerHTML = 'body.focus-action-up>*{transform:translate(0,-88vw);-webkit-transform:translate(0,-88vw)}';
     document.head.appendChild(style);
 
     // 安卓弹出键盘发生窗口尺寸变化后元素在屏幕外就让元素滚动回可视区域
     window.addEventListener('resize', function () {
       waitResize = false; // 触发resize后下面的事件就不用等待
-      var inputElem = document.querySelector(':focus');
+      var inputElem = document.body.querySelector(':focus');
       if (inputElem) {
         setTimeout(function () {
           if (inputElem.getBoundingClientRect().bottom > document.documentElement.clientHeight)
             inputElem.scrollIntoView(false);
-        }, 330);
+        }, 300);
       }
     });
 
@@ -61,12 +62,13 @@ window.addEventListener('DOMContentLoaded', function () {
       } else {
         waitResize = true; // 开始等待resize事件
         setTimeout(function () {
-          if (waitResize && e.target.getBoundingClientRect().top > window.innerHeight / 2) { // 未等到resize事件且元素在下半屏幕
-            waitResize = false; // 不用等resize了...
-            document.body.scrollTop += window.innerHeight / 3;
-            if (e.target.getBoundingClientRect().top > window.innerHeight / 2) {
+          if (e.target.getBoundingClientRect().top > window.innerHeight / 2) { // 未等到resize事件且元素在下半屏幕
+            document.body.scrollTop += e.target.getBoundingClientRect().top / 1.8;
+            if (waitResize && e.target.getBoundingClientRect().top > window.innerHeight / 1.9) {
+              // 不用等resize了...
               document.body.classList.add('focus-action-up');
             }
+            waitResize = false;
           }
         }, 500);
       }
@@ -75,12 +77,9 @@ window.addEventListener('DOMContentLoaded', function () {
     // 一个元素失去焦点后检测是否还有焦点元素，没有的话删除填充底部的样式
     document.addEventListener('focusout', function () {
       setTimeout(function () {
-        waitResize = false; // 切换input不监听resize,因为有resize的接口切换的时候不会再触发resize
-        if (!document.querySelector(':focus'))
+        if (!document.body.querySelector(':focus'))
           document.body.classList.remove('focus-action-up');
       }, 9);
     });
   }
-});
-
-
+}, 0);
